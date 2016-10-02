00204a819fde0d07d43e1ea39704bb9430bb4c08
Harri Heljala <harri.heljala@gmail.com>
2016-10-02 23:34:22 +0300

    Author section ready.

--- template-parts/front-page-author-bio-container-small.php
+++ template-parts/front-page-author-bio-container-small.php
@@ -1,5 +1,5 @@
 <div class="grid-item grid-item--author col-xs-12 visible-xs-block">
-	<div class="grid-item__content">
+	<div class="grid-item__content grid-item__content--long">
 		<?php get_template_part( 'template-parts/front-page', 'author-bio-content' ); ?>
 	</div>
 </div>
--- template-parts/front-page-author-bio-content.php
+++ template-parts/front-page-author-bio-content.php
@@ -1,16 +1,30 @@
-<div class="grid-item__content__wrapper">
-	<img src="<?php echo get_template_directory_uri() ?>/images/harri-heljala.jpg" class="avatar img-responsive center-block" />
-	<h1>Harri Heljala</h1>
-	<p>consectetur adipisicing elit. Adipisci eum et animi, doloremque ratione consequuntur optio deserunt ullam beatae. Nulla pariatur neque eum expedita repudiandae, unde dolor aliquam quas blanditiis.</p>
+<?php
+	$author = get_user_by( 'email', get_bloginfo( 'admin_email' ) );
+	$name = $author->first_name . ' ' . $author->last_name;
+?>
+
+<aside class="grid-item__content__wrapper">
+	<img src="<?php echo get_avatar_url( $author->ID, array( 'size' => 400 ) ); ?>" alt="<?php echo $name; ?>" title="<?php echo $name; ?>" class="avatar img-responsive center-block" />
+	<h1><?php echo $name; ?></h1>
+
+	<?php if ( defined( 'WPPT_AUTHOR_LINKS' ) ) : ?>
+		<ul class="author-links">
+			<?php foreach ( json_decode( WPPT_AUTHOR_LINKS ) as $link ) : ?>
+				<li><a href="<?php echo $link->href ?>" target="_blank" title="<?php echo $name; ?> on <?php echo $link->name ?>"><i class="fa fa-<?php echo $link->icon ?>" aria-hidden="true"></i></a></li>
+			<?php endforeach; ?>
+		</ul>
+	<?php endif; ?>
+
+	<p><?php echo $author->description; ?></p>
 
 	<?php
 		$tags = get_tags();
 		if ( ! empty( $tags ) ) :
 	?>
-		<ul>
+		<ul class="tag-list">
 			<?php foreach ( $tags as $tag ) : ?>
-				<li><a href="<?php echo get_tag_link( $tag->term_id ); ?>"><?php echo $tag->name ?></a></li>
+				<li><a href="<?php echo get_tag_link( $tag->term_id ); ?>" title="All <?php echo $name; ?>'s Portfolio Items Tagged as <?php echo $tag->name ?>">#<?php echo $tag->name ?></a></li>
 			<?php endforeach; ?>
 		</ul>
 	<?php endif; ?>
-</div>
+</aside>
