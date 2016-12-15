8af683200b56b3b53192a4423ce22b15f466e06e
Harri Heljala <harri.heljala@gmail.com>
2016-12-15 12:37:04 +0200

    Optimizing sidebar link attributes.

--- template-parts/global-author-bio-content.php
+++ template-parts/global-author-bio-content.php
@@ -4,7 +4,7 @@
 ?>
 
 <aside class="grid-item__content__wrapper text-center">
-	<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo get_bloginfo( 'name' ); ?>" rel="author" class="home-link">
+	<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo get_bloginfo( 'name' ); ?>" rel="author me" class="home-link">
 		<img src="<?php echo get_avatar_url( $author->ID, array( 'size' => 400 ) ); ?>" alt="<?php echo $name; ?>" class="avatar img-responsive center-block" />
 
 		<?php if ( is_single() || is_page() ) : ?>
@@ -17,7 +17,7 @@
 	<?php if ( defined( 'WPPT_AUTHOR_LINKS' ) ) : ?>
 		<ul class="author-links">
 			<?php foreach ( json_decode( WPPT_AUTHOR_LINKS ) as $link ) : ?>
-				<li><a href="<?php echo $link->href ?>" rel="author" target="_blank" title="<?php echo $name; ?> on <?php echo $link->name ?>"><i class="fa fa-<?php echo $link->icon ?>" aria-hidden="true"></i></a></li>
+				<li><a href="<?php echo $link->href ?>" rel="author me noreferrer noopener" target="_blank" title="<?php echo $name; ?> on <?php echo $link->name ?>"><i class="fa fa-<?php echo $link->icon ?>" aria-hidden="true"></i><span class="screen-reader-text"><?php echo $name; ?> on <?php echo $link->name ?></span></a></li>
 			<?php endforeach; ?>
 		</ul>
 	<?php endif; ?>
@@ -30,7 +30,7 @@
 	?>
 		<ul class="tag-list">
 			<?php foreach ( $tags as $tag ) : ?>
-				<li><a href="<?php echo get_tag_link( $tag->term_id ); ?>" rel="tag" title="All <?php echo $name; ?>'s Portfolio Items Tagged as <?php echo $tag->name ?>">#<?php echo $tag->name ?></a></li>
+				<li><a href="<?php echo get_tag_link( $tag->term_id ); ?>" rel="tag" title="All <?php echo $name; ?>'s Portfolio Items Tagged as <?php echo $tag->name ?>"><span class="screen-reader-text">All <?php echo $name; ?>'s Portfolio Items Tagged as </span>#<?php echo $tag->name ?></a></li>
 			<?php endforeach; ?>
 		</ul>
 	<?php endif; ?>
