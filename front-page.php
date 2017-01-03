f1c9a49c161736e12cf9c98f279f18f0d20e20ab
Harri Heljala <harri.heljala@gmail.com>
2017-01-03 18:48:40 +0200

    Adding comments.

--- front-page.php
+++ front-page.php
@@ -5,26 +5,52 @@
 		<?php get_template_part( 'template-parts/front-page', 'author-bio-container-small' ); ?>
 
 		<?php
+
+			/**
+			 * Get the items under the main categories to show on front-page
+			 */
 			if ( defined( 'WPPT_MAIN_CATEGORIES' ) ) {
 
-				$categories = get_categories( array(
-					'include' => WPPT_MAIN_CATEGORIES,
-					'orderby' => 'include'
-				));
+				// To count the number of categories, explode the string if needed
+				if ( is_array( WPPT_MAIN_CATEGORIES ) ) {
+					$include = WPPT_MAIN_CATEGORIES;
+				} else {
+					$include = explode( WPPT_MAIN_CATEGORIES, ',' );
+				}
 
+				// Create the query to get the categories
+				$query = array(
+					'include' => $include
+				);
+
+				// If more than one categories, use the order from WPPT_MAIN_CATEGORIES
+				if ( count( $include ) > 1 ) {
+					$query['orderby'] = 'include';
+				}
+
+				// Get categories
+				$categories = get_categories( $query );
+
+				// Loop through the categories
 				foreach ( $categories as $category_key => $category ) {
 
+					// Insert the category name
 					include( locate_template( 'template-parts/front-page-category.php' ) );
 
+					// Insert the author bio if first category
 					if ( 0 === $category_key ) {
 						get_template_part( 'template-parts/front-page', 'author-bio-container-medium' );
 					}
 
+					// Create custom query to get the posts from the category
 					global $query_string;
 					query_posts( $query_string . '&cat=' . $category->term_id );
+
+					// Insert loop for this category
 					include( locate_template( 'template-parts/global-the-loop.php' ) );
 				}
 			} else {
+				// If no main categories defined show the normal loop
 				include( locate_template( 'template-parts/global-the-loop.php' ) );
 			}
 		?>
--- functions.php
+++ functions.php
@@ -1,33 +1,55 @@
 <?php
 
+// Support for featured images
 add_theme_support( 'post-thumbnails' );
 
+// Add the hooks to enqueue styles and scripts
 add_action( 'wp_enqueue_scripts', 'wppt_enqueue_styles' );
 add_action( 'wp_enqueue_scripts', 'wppt_enqueue_scripts' );
 
+/**
+ * Enqueue theme styles
+ */
 function wppt_enqueue_styles() {
+
+	// Dependencies
 	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/node_modules/bootswatch/paper/bootstrap.css' );
 	wp_enqueue_style( 'animate', get_template_directory_uri() . '/node_modules/animate.css/animate.css' );
 	wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/node_modules/font-awesome/css/font-awesome.css' );
 	wp_enqueue_style( 'owl.carousel', get_template_directory_uri() . '/node_modules/owlcarousel/owl-carousel/owl.carousel.css' );
 	wp_enqueue_style( 'owl.theme', get_template_directory_uri() . '/node_modules/owlcarousel/owl-carousel/owl.theme.css', array( 'owl.carousel' ) );
 
+	// Theme specific styles
 	wp_enqueue_style( 'overwrites', get_template_directory_uri() . '/styles/overwrites.css' );
 	wp_enqueue_style( 'grid', get_template_directory_uri() . '/styles/grid.css' );
 	wp_enqueue_style( 'single', get_template_directory_uri() . '/styles/single.css' );
 	wp_enqueue_style( 'main', get_template_directory_uri() . '/styles/main.css', array( 'bootstrap', 'animate', 'font-awesome', 'owl.carousel', 'owl.theme', 'overwrites', 'grid', 'single' ) );
 }
 
+/**
+ * Enqueue theme scripts
+ */
 function wppt_enqueue_scripts() {
+
+	// Dependencies
 	wp_enqueue_script( 'masonry.pkgd', get_template_directory_uri() . '/node_modules/masonry-layout/dist/masonry.pkgd.js', array( 'jquery' ) );
 	wp_enqueue_script( 'owl.carousel', get_template_directory_uri() . '/node_modules/owlcarousel/owl-carousel/owl.carousel.js', array( 'jquery' ) );
+
+	// Theme specific scripts
 	wp_enqueue_script( 'main', get_template_directory_uri() . '/scripts/main.js', array( 'jquery', 'owl.carousel', 'masonry.pkgd' ) );
 }
 
+/**
+ * Echo the transparent button class based on if there is a background image
+ */
 function wppt_the_transparent_button_class() {
 	echo wppt_get_transparent_button_class();
 }
 
+/**
+ * Return the transparent button class based on if there is a background image
+ * @return string Transparent button class
+ */
 function wppt_get_transparent_button_class() {
 	$class = 'transparent-button';
 
@@ -38,10 +60,17 @@ function wppt_get_transparent_button_class() {
 	}
 }
 
+/**
+ * Echo the grid item background class based on if there is a background image
+ */
 function wppt_the_grid_item_background_class() {
 	echo wppt_get_grid_item_background_class();
 }
 
+/**
+ * Return the grid item background class based on if there is a background image
+ * @return string Grid item background class
+ */
 function wppt_get_grid_item_background_class() {
 	if ( has_post_thumbnail() ) {
 		return 'grid-item--has-background';
@@ -50,10 +79,17 @@ function wppt_get_grid_item_background_class() {
 	}
 }
 
+/**
+ * Echo the grid item background style based on bg-image setttings of the post
+ */
 function wppt_the_grid_item_background_style() {
 	echo wppt_get_grid_item_background_style();
 }
 
+/**
+ * Return the grid item background style based on bg-image setttings of the post
+ * @return string Grid item background style
+ */
 function wppt_get_grid_item_background_style() {
 
 	if ( ! has_post_thumbnail() ) {
@@ -80,6 +116,10 @@ function wppt_get_grid_item_background_style() {
 	return $background . '";';
 }
 
+/**
+ * Return the blog owner user object
+ * @return (WP_User|false WP_User object on success, false on failure.
+ */
 function wppt_get_author() {
 	return get_user_by( 'email', get_bloginfo( 'admin_email' ) );
 }
